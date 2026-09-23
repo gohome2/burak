beab53f9681b6f0770d7dd941c6f710a9e6ea005
gohome2 <threed484@gmail.com>
2026-09-23 20:41:05 +0900

    feat: integration of eviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
-console.log("EXECUTED");
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment";
+console.log("PORT:", process.env.PORT);
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-const person: string = "JOHN";
-const count: number = 100;
+console.log("MONGO_URL:", process.env.PORT);
