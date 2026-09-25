7323dfed6cb3b9d29203c03931efbd54944054ed
gohome2 <threed484@gmail.com>
2026-09-25 20:36:45 +0900

    feat: MIT task added

--- src/train.ts
+++ src/train.ts
@@ -1,13 +1,25 @@
-type SquareNumber = {
-  number: number;
-  square: number;
-};
+//---------N Task--------//
 
-function getSquareNumbers(numbers: number[]): SquareNumber[] {
-  return numbers.map((number): SquareNumber => {
-    return {
-      number: number,
-      square: number * number,
-    };
-  });
+function palindromCheck(word: string): boolean {
+  const reversedWord: string = word.split("").reverse().join("");
+
+  return word === reversedWord;
 }
+
+console.log(palindromCheck("dad")); // true
+console.log(palindromCheck("son")); // false
+
+//
+// type SquareNumber = {
+//   number: number;
+//   square: number;
+// };
+
+// function getSquareNumbers(numbers: number[]): SquareNumber[] {
+//   return numbers.map((number): SquareNumber => {
+//     return {
+//       number: number,
+//       square: number * number,
+//     };
+//   });
+// }
